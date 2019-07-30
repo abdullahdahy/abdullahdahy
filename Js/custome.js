503cbc2311732ade433aa3b8f997ca8c80bdf27e
abdullahdahy <abdullahdahy@github.com>
2019-07-30 14:25:47 +0200

    1

--- Js/custome.js
+++ Js/custome.js
@@ -1,95 +1,79 @@
-/* global $, alert, consol*/
+/* global, $, alert, consol */
 
 $(document).ready(function () {
     
     'use strict';
     
-    //adjust header height
-    
-    var myHeader = $('.header');
-    var mySlider = $('.slider');
-    
-    myHeader.height($(window).height());
-    
-    $(window).resize(function () {
-        
-        myHeader.height($(window).height());
-        
-    });
-    
-    // links add active class
-    
-    $('.links li a').hover(function () {
-       
-        $(this).parent().addClass('active').siblings().removeClass('active');
-        
-    });
-    
-    // adjust slider
-    mySlider.each(function () {
-        $(this).css('paddingTop', ($(window).height() - $('.slider li').height()) / 2);
-    });
-    
-    // trigger slider
-    mySlider.bxSlider();
-    
-    // scroll
-    $('.links li a').click(function () {
-        $('html, body').animate({
-            scrollTop: $('#' + $(this).data('value')).offset().top
-        }, 1000);
-    });
-    
-    
-    // t-slider
-    (function autoSlider() {
-        
-        $('.t-slider .active').each(function () {
-            
-            if (!$(this).is(':last-child')) {
-                
-                $(this).delay(3000).fadeOut(1000, function () {
-                    $(this).removeClass('active').next().addClass('active').fadeIn();
-                    autoSlider();
-                });
-                
-            } else {
-                $(this).delay(3000).fadeOut(1000, function () {
-                    $(this).removeClass('active');
-                    $('.t-slider div').eq(0).addClass('active').fadeIn();
-                    autoSlider();
-                });
-                
-            }
-        });
-        
-    }());
-    
-    // mixitup
-
-	$('#Container').mixItUp();
-    
-    //adjust shuffle
-    $('.shuffle li').click(function () {
-        $(this).addClass('selected').siblings().removeClass('selected');
-    });
-    
-    
     //scroll
     
     $(function () {
-        $("html").niceScroll( {
-            cursorcolor: '#1abc9c',
+        $("html").niceScroll({
+            cursorcolor: '#f7600e',
             cursorwidth: '10px',
             cursorborder: 'none'
         });
     });
     
+    //adjust header height
+    
+    var mySlider = $('.slider');
+    
+    $('.header').height($(window).height());
+    
+    $(window).resize(function () {
+        
+        $('.header').height($(window).height());
+        
+    });
     
     
+    // arrow
+    $('.header .arrow i').click(function () {
+        
+        $('html, body').animate({
+            scrollTop: $('.features').offset().top
+        }, 1000);
+        
+    });
+    
+    // show hidden
+    $('.our-work .show-more').click(function () {
+        $('.our-work .hidden').fadeIn(2000);
+    });
     
     
+    // check testimonials
     
+    var leftArrow = $('.testim .fa-chevron-left'),
+        
+        rightArrow = $('.testim .fa-chevron-right');
+    function checkClients() {
+        if ($('.client:first').hasClass('active')) {
+            leftArrow.fadeOut();
+        } else {
+            leftArrow.fadeIn();
+        }
+        if ($('.client:last').hasClass('active')) {
+            rightArrow.fadeOut();
+        } else {
+            rightArrow.fadeIn();
+        }
+    }
+    checkClients();
+    
+    $('.testim i').click(function () {
+        if ($(this).hasClass('fa-chevron-right')) {
+            $('.testim .active').fadeOut(100, function () {
+                $(this).removeClass('active').next('.client').addClass('active').fadeIn();
+                checkClients();
+            });
+        } else {
+            $('.testim .active').fadeOut(100, function () {
+                $(this).removeClass('active').prev('.client').addClass('active').fadeIn();
+                checkClients();
+            });
+        }
+    });
     
     
 });
